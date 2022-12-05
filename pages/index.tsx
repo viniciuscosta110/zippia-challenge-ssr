f8bc7412104412c9cbda38149bb4a6f382dda4ea
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-12-05 10:16:36 -0300

    Refactor: set index to static

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 
-export default function Home() {
+export default function Home({...props}) {
   return (
     <h2>
       <Link href={'/test/jobs'}>Go to Jobs</Link>
     </h2>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  }
 }
